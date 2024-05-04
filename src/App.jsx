a96268535f70f11753ca9b5655714c4e530c50c7
evamraposo <evamarinaraposo@gmail.com>
2024-05-04 19:45:32 -0300

    feat: alteras cores dos botões, acrescenta imagem do site, cria a página home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "./components/login/Login";
 import Cadastro from "./components/cadastro/Cadastro";
+import Home from "./components/home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Recovery from "./components/recovery/Recovery";
 
@@ -9,6 +10,7 @@ function App(){
     return(
         <BrowserRouter>
             <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/cadastro" element={<Cadastro />} />
             <Route path="/recovery" element={<Recovery />} />
