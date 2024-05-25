0ef1fe53295fa18a4454ecf78b9e2f02615f90c1
Débora Carvalho <debora.sesv@gmail.com>
2024-05-25 13:59:11 -0300

    parte da interface pronta

--- src/components/crud-user/user.tsx
+++ src/components/crud-user/user.tsx
@@ -3,7 +3,7 @@ import { Button } from './button';
 import '../../styles/global.css';
 import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle, faEdit, faTrashAlt, faKey } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, faEdit, faTrashAlt, faKey, faCity, faMapMarkerAlt, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { profiler } from '../../services/users/profileUser';
 import { deleteUser } from '../../services/users/profileDelete';
 
@@ -19,7 +19,7 @@ interface User {
   state: UserState;
 }
 
-export const CrudUser = ({ accessToken }: { accessToken: string }) => {
+export const CrudUser = ({ accessToken, onBackToHome }: { accessToken: string, onBackToHome: () => void }) => {
   const [user, setUser] = useState<User>({
     name: { value: 'João', editing: false },
     password: { value: '', editing: false },
@@ -63,7 +63,7 @@ export const CrudUser = ({ accessToken }: { accessToken: string }) => {
 
   const handleDeleteUser = () => {
     deleteUser(accessToken)
-      .then((data) => {
+      .then(() => {
         localStorage.removeItem('token');
         setUser({
           name: { value: '', editing: false },
@@ -78,98 +78,108 @@ export const CrudUser = ({ accessToken }: { accessToken: string }) => {
   };
 
   return (
-    <div className='profile-container'>
-      <div className='input-container'>
-        <h1>Perfil Usuário</h1>
-        <label>Nome</label>
-        <div className='icon-input'>
-          <FontAwesomeIcon icon={faUserCircle} style={{ color: '#d3d3d3' }} size='2x' className='icon' />
-          <input
-            id='name'
-            type='text'
-            value={user.name.value}
-            onChange={(e) => handleInputChange(e, 'name')}
-            disabled={!user.name.editing}
-            className='input'
-            placeholder='Nome'
-          />
-          <FontAwesomeIcon
-            icon={faEdit}
-            style={{ color: '#d3d3d3', cursor: 'pointer', marginLeft: '1rem' }}
-            size='2x'
-            className='icon'
-            onClick={() => handleEditClick('name')}
-          />
+    <div className='background'>
+      <div className='profile-container'>
+        <div className='input-container'>
+          <h1>Perfil do Usuário</h1>
+
+          <label>Nome</label>
+          <div className='icon-input'>
+            <FontAwesomeIcon icon={faUserCircle} style={{ color: '#d3d3d3' }} size='2x' className='icon' />
+            <input
+              id='name'
+              type='text'
+              onChange={(e) => handleInputChange(e, 'name')}
+              disabled={!user.name.editing}
+              className='input'
+              placeholder='Nome'
+            />
+            <FontAwesomeIcon
+              icon={faEdit}
+              style={{ color: '#d3d3d3', cursor: 'pointer', marginLeft: '1rem' }}
+              size='2x'
+              className='icon'
+              onClick={() => handleEditClick('name')}
+            />
+          </div>
+
+          <label>Cidade</label>
+          <div className='icon-input'>
+            <FontAwesomeIcon icon={faCity} style={{ color: '#d3d3d3' }} size='2x' className='icon' />
+            <input
+              id='city'
+              type='text'
+              value={user.city.value}
+              onChange={(e) => handleInputChange(e, 'city')}
+              disabled={!user.city.editing}
+              className='input'
+              placeholder='Cidade'
+            />
+            <FontAwesomeIcon
+              icon={faEdit}
+              style={{ color: '#d3d3d3', cursor: 'pointer', marginLeft: '1rem' }}
+              size='2x'
+              className='icon'
+              onClick={() => handleEditClick('city')}
+            />
+          </div>
+
+          <label>Estado</label>
+          <div className='icon-input'>
+            <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: '#d3d3d3' }} size='2x' className='icon' />
+            <input
+              id='state'
+              type='text'
+              value={user.state.value}
+              onChange={(e) => handleInputChange(e, 'state')}
+              disabled={!user.state.editing}
+              className='input'
+              placeholder='Estado'
+            />
+            <FontAwesomeIcon
+              icon={faEdit}
+              style={{ color: '#d3d3d3', cursor: 'pointer', marginLeft: '1rem' }}
+              size='2x'
+              className='icon'
+              onClick={() => handleEditClick('state')}
+            />
+          </div>
+
+          <label>Senha</label>
+          <div className='icon-input'>
+            <FontAwesomeIcon icon={faKey} style={{ color: '#d3d3d3' }} size='2x' className='icon' />
+            <input
+              type='password'
+              value={user.password.value}
+              onChange={(e) => handleInputChange(e, 'password')}
+              disabled={!user.password.editing}
+              placeholder='Senha'
+              className='input'
+            />
+            <FontAwesomeIcon
+              icon={faEdit}
+              style={{ color: '#d3d3d3', cursor: 'pointer', marginLeft: '1rem' }}
+              size='2x'
+              className='icon'
+              onClick={() => handleEditClick('password')}
+            />
+          </div>
         </div>
 
-        <label>Cidade</label>
-        <div className='icon-input'>
-          <input
-            id='city'
-            type='text'
-            value={user.city.value}
-            onChange={(e) => handleInputChange(e, 'city')}
-            disabled={!user.city.editing}
-            className='input'
-            placeholder='Cidade'
-          />
-          <FontAwesomeIcon
-            icon={faEdit}
-            style={{ color: '#d3d3d3', cursor: 'pointer', marginLeft: '1rem' }}
-            size='2x'
-            className='icon'
-            onClick={() => handleEditClick('city')}
-          />
+        <div className='style-button'>
+          <Button onClick={handleSaveEdit}>Salvar Alterações</Button>
+          <span style={{ margin: '0 34px' }}></span>
+          <Button onClick={handleDeleteUser}>
+            <FontAwesomeIcon icon={faTrashAlt} style={{ marginRight: '0.5rem' }} />
+            Excluir Conta
+          </Button>
         </div>
-
-        <label>Estado</label>
-        <div className='icon-input'>
-          <input
-            id='state'
-            type='text'
-            value={user.state.value}
-            onChange={(e) => handleInputChange(e, 'state')}
-            disabled={!user.state.editing}
-            className='input'
-            placeholder='Estado'
-          />
-          <FontAwesomeIcon
-            icon={faEdit}
-            style={{ color: '#d3d3d3', cursor: 'pointer', marginLeft: '1rem' }}
-            size='2x'
-            className='icon'
-            onClick={() => handleEditClick('state')}
-          />
+        <div className='back-to-home-button'>
+          <Button onClick={onBackToHome}>
+            <FontAwesomeIcon icon={faArrowLeft} style={{ marginRight: '1rem' }} />
+            Retornar para a Tela Inicial
+          </Button>
         </div>
-
-        <label>Senha</label>
-        <div className='icon-input'>
-          <FontAwesomeIcon icon={faKey} style={{ color: '#d3d3d3' }} size='2x' className='icon' />
-          <input
-            type='password'
-            value={user.password.value}
-            onChange={(e) => handleInputChange(e, 'password')}
-            disabled={!user.password.editing}
-            placeholder='Senha'
-            className='input'
-          />
-          <FontAwesomeIcon
-            icon={faEdit}
-            style={{ color: '#d3d3d3', cursor: 'pointer', marginLeft: '1rem' }}
-            size='2x'
-            className='icon'
-            onClick={() => handleEditClick('password')}
-          />
-        </div>
-      </div>
-
-      <div className='style-button'>
-        <Button onClick={handleSaveEdit}>Salvar Alterações</Button>
-        <span style={{ margin: '0 30px' }}></span>
-        <Button onClick={handleDeleteUser}>
-          <FontAwesomeIcon icon={faTrashAlt} style={{ marginRight: '0.5rem' }} />
-          Deletar Conta
-        </Button>
       </div>
     </div>
   );
